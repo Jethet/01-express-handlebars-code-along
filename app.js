5e9a317316d91bf4872c9797da4a1f113a2da21f
Jethet <henriette.hettinga@gmail.com>
2020-02-06 11:59:00 +0100

    Add first structure for app.js code

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+const PORT = 3000;
+const movies = require('./moviesData');
+
+//SET TEMPLATING ENGINE
+
+
+//SET VIEWS DIRECTORY
+
+
+//SET FOLDER FOR STATIC FILES
+
+//ROUTES
+
+//START THE SERVER
