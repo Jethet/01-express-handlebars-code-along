02a8ec2e605259b5f8ac7a5ec5e8194afda8eeee
Jethet <henriette.hettinga@gmail.com>
2020-02-06 12:05:24 +0100

    Write basic code to run server

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ const PORT = 3000;
 const movies = require('./moviesData');
 
 //SET TEMPLATING ENGINE
-
+app.set('view engine', 'hbs');
 
 //SET VIEWS DIRECTORY
-
+app.set('views', __dirname + '/views');
 
 //SET FOLDER FOR STATIC FILES
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 //ROUTES
 
 //START THE SERVER
+app.listen(PORT, () => {
+    console.log(`Server is running`);
+});
